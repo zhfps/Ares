a2ff92c68e63c0e6f476bd1bace6b78a760ca554
zhfps <it_dog_zhang@163.com>
2024-05-08 22:55:38 +0800

    change

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './samples/node-api'
 import './index.css'
+import { FluentProvider, teamsLightTheme } from '@fluentui/react-components'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <FluentProvider theme={teamsLightTheme}>
+            <App />
+        </FluentProvider>,
+    </React.StrictMode>,
 )
 
 postMessage({ payload: 'removeLoading' }, '*')
