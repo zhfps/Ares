3ee657c699d4986ac74fa824785185b990102e33
zhfps <it_dog_zhang@163.com>
2024-05-10 22:33:34 +0800

    add fluentui

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -3,16 +3,6 @@ import { release } from 'node:os'
 import { join } from 'node:path'
 import { update } from './update'
 
-// The built directory structure
-//
-// ├─┬ dist-electron
-// │ ├─┬ main
-// │ │ └── index.js    > Electron-Main
-// │ └─┬ preload
-// │   └── index.js    > Preload-Scripts
-// ├─┬ dist
-// │ └── index.html    > Electron-Renderer
-//
 process.env.DIST_ELECTRON = join(__dirname, '../')
 process.env.DIST = join(process.env.DIST_ELECTRON, '../dist')
 process.env.VITE_PUBLIC = process.env.VITE_DEV_SERVER_URL
@@ -47,9 +37,6 @@ async function createWindow() {
         icon: join(process.env.VITE_PUBLIC, 'favicon.ico'),
         webPreferences: {
             preload,
-            // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
-            // Consider using contextBridge.exposeInMainWorld
-            // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
             nodeIntegration: true,
             contextIsolation: false,
         },
@@ -58,7 +45,7 @@ async function createWindow() {
     if (url) { // electron-vite-vue#298
         win.loadURL(url)
         // Open devTool if the app is not packaged
-        win.webContents.openDevTools()
+        // win.webContents.openDevTools()
     } else {
         win.loadFile(indexHtml)
     }
